ab4b25f6ecfbb883fe8eb372ea7a1f0b3072701a
Colin Devroe <colin@cdevroe.com>
2016-10-08 11:16:51 -0400

    Added version for user style to help with caching. Updated theme version. Fixed search form PHP error.

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
-<form role="search" method="get" class="form-inline search-form" action="' . esc_url( home_url( '/' ) ) . '">
+<form role="search" method="get" class="form-inline search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
     <label>
         <span class="screen-reader-text"><?php _x( 'Search for:', 'label' ); ?></span>
         <input type="search" class="form-control search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
